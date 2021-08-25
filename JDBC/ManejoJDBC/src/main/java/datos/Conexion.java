9497b97f1c56785da5fb909afc55acbeaf6e51ed
Stefany <stefanymedina25@gmail.com>
2021-08-25 16:26:39 -0500

    add all methods to the conxion file

--- JDBC/ManejoJDBC/src/main/java/datos/Conexion.java
+++ JDBC/ManejoJDBC/src/main/java/datos/Conexion.java
@@ -5,10 +5,35 @@
  */
 package datos;
 
+import java.sql.*;
+
 /**
  *
  * @author stefanymedina
  */
-public class Conexion { //test - 
+public class Conexion { //test -  sf
     private static final String JDBC_URL = "jdbc:mysql://127.0.0.1:3306/test?useSSL=false&useTimezone=true&erverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "1234";
+    
+    
+    public static Connection getConnection() throws SQLException{
+       return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+    }
+    
+    public static void close(ResultSet rs) throws SQLException{
+        rs.close();
+    }
+    
+    public static void close(PreparedStatement smtm) throws SQLException{
+        smtm.close();
+    }
+    
+    public static void close(Statement smtm) throws SQLException{
+        smtm.close();
+    }
+    
+    public static void close(Connection conn) throws SQLException{
+        conn.close();
+    }
 }
